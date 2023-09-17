c6bb977127bad9d9fa935fa1547ce995e9aa5d00
Yao Wenhao <yaowenhao@hyperchain.cn>
2023-09-17 16:39:18 +0800

    fix(vm): add engine

--- common/vm.go
+++ common/vm.go
@@ -12,14 +12,14 @@ type TxContext struct {
 
 // TxIndex is the unique index of a transaction (`vm.Run`)
 type TxIndex struct {
-	EngineIdx int64 // EngineIdx is the index of engine (maybe support multiple test stage in future)
-	TxIdx     int64 // TxIdx is the index of sent transaction
-	MissIdx   int64 // MissIdx is the index of missed transaction
+	TxIdx   int64 // TxIdx is the index of sent transaction
+	MissIdx int64 // MissIdx is the index of missed transaction
 }
 
 // VMContext is the context of vm
 type VMContext struct {
 	WorkerIdx int64 `mapstructure:"worker"`   // WorkerIdx is the index of worker
 	VMIdx     int64 `mapstructure:"vm"`       // VMIdx is the index of vm
+	Engine    int64 `mapstructure:"engine"`   // Engine is the total engine(vm) num
 	Accounts  int64 `mapstructure:"accounts"` // Accounts is the count of acutal test accounts
 }
